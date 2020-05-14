96a6077db6c52ea0e2dbe10b17f4c668e6e76928
h1d3mun3 74k4h45h1 <h1d3mun3.74k4h45h1@gmail.com>
2020-05-14 17:54:25 +0900

    Initial Commit

---
+++ InsetsTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InsetsTestApp
+//
+//  Created by h1d3mun3-74k4h45h1 on 5/14/20.
+//  Copyright © 2020 h1d3mun3-74k4h45h1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
